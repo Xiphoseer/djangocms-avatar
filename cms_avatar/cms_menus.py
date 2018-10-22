aa3fb44c8c454151f951363439ec99835d20a05e
Daniel Schweighöfer <SchweighoeferD@gmail.com>
2018-10-22 15:23:37 +0200

    Initial commit

---
+++ cms_avatar/cms_menus.py
@@ -0,0 +1,19 @@
+from menus.base import NavigationNode
+from menus.menu_pool import menu_pool
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+from cms.menu_bases import CMSAttachMenu
+
+class AvatarMenu(CMSAttachMenu):
+
+    name = _("Avatar Menu")
+
+    def get_nodes(self, request):
+        nodes = []
+        n1 = NavigationNode(_('Add'), reverse('avatar:avatar_add'),1)
+        n2 = NavigationNode(_('Change'), reverse('avatar:avatar_change'),2)
+        n3 = NavigationNode(_('Delete'), reverse('avatar:avatar_delete'),3)
+        nodes += [n1,n2,n3]
+        return nodes
+
+menu_pool.register_menu(AvatarMenu)
